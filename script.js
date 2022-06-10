5f9b1a52a138bdb8781c511e0ac8a29ad288722f
amiralipaak <amiralipaak380@gmail.com>
2022-06-10 16:17:04 +0430

    finished the project, will add commentary

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const alphabetLetters = document.getElementById("letters");
 let randomItem = "";
 let clicked = [];
 let result = "";
-
+let mistakes = 0;
 function selectRandomItem() {
   randomItem = secretPhrases[Math.floor(Math.random() * secretPhrases.length)];
   alphabetLetters.addEventListener("click", buttonHandler);
+  window.addEventListener("keydown", keyHandler);
   console.log(randomItem);
 }
 
@@ -27,7 +28,20 @@ function checkIfWon() {
       "assets/winner.png";
   }
 }
-
+function checkIfLost() {
+  if (mistakes === 6) {
+    document.getElementById("gameover").querySelector("p").style.display =
+      "block";
+    document.getElementById(
+      "clue"
+    ).innerHTML = `<p> The Word was : ${randomItem}</p>`;
+  }
+}
+function updateHangmanPicture() {
+  const image = document.getElementById("image").querySelector("img");
+  image.src = `assets/hangman${mistakes}.png`;
+  image.alt = "Stop Trying, Its Over";
+}
 function letterHandler(letter) {
   letter = letter.toLowerCase();
   clicked.indexOf(letter) === -1 ? clicked.push(letter) : null;
@@ -36,11 +50,19 @@ function letterHandler(letter) {
     setUnderScores();
     checkIfWon();
   } else if (randomItem.indexOf(letter) === -1) {
+    mistakes++;
+    checkIfLost();
+    updateHangmanPicture();
   }
 }
 
 function buttonHandler(event) {
   letterHandler(event.target.id);
 }
+
+function keyHandler(event) {
+  letterHandler(event.key);
+}
+
 selectRandomItem();
 setUnderScores();
